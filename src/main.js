dca19afc7ad56bd2ec20306e6d2128b8b0df5fea
jeffbai996 <jeff.bai996@gmail.com>
2026-07-03 16:44:10 -0700

    chore: tear down v2 app for v3 rebuild

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './styles/main.css'
-import { initShell } from './layout/shell.js'
-import { initRouter } from './router.js'
-import { maybeShowOnboarding } from './layout/onboarding.js'
-
-const app = document.getElementById('app')
-initShell(app)
-initRouter()
-maybeShowOnboarding()
